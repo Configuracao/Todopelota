bc3b3c032d634248b934167311f24ef1e486e705
ALEXVIP4G <110699395+Configuracao@users.noreply.github.com>
2025-05-05 16:23:02 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function playChannel(url) {
+  const video = document.getElementById('video');
+
+  if (Hls.isSupported()) {
+    const hls = new Hls();
+    hls.loadSource(url);
+    hls.attachMedia(video);
+    hls.on(Hls.Events.MANIFEST_PARSED, () => {
+      video.play();
+    });
+  } else if (video.canPlayType('application/vnd.apple.mpegurl')) {
+    video.src = url;
+    video.addEventListener('loadedmetadata', () => {
+      video.play();
+    });
+  } else {
+    alert('Tu navegador no es compatible con reproducción HLS.');
+  }
+}
